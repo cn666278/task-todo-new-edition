b295e17fc60b1f209b53a8e9c071b5efe98fe8d8
Kurumi Saiko <cst1909128@xmu.edu.my>
2023-05-10 19:54:27 +0800

    (Cont)Add Page - All Task Page Add category list in AllTaskPage()

---
+++ lib/ui/screens/all_task_page.dart
@@ -0,0 +1,407 @@
+import 'package:date_picker_timeline/date_picker_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:todo_app_new_edition/controllers/task_controller.dart';
+import 'package:todo_app_new_edition/db/db_helper.dart';
+import 'package:todo_app_new_edition/models/mysql.dart';
+import 'package:todo_app_new_edition/models/task.dart';
+import 'package:todo_app_new_edition/services/notification_services.dart';
+import 'package:todo_app_new_edition/services/theme_services.dart';
+import 'package:todo_app_new_edition/ui/entry_point.dart';
+import 'package:todo_app_new_edition/ui/screens/all_task.dart';
+import 'package:todo_app_new_edition/ui/screens/calendar.dart';
+import 'package:todo_app_new_edition/ui/screens/calendar_page.dart';
+import 'package:todo_app_new_edition/ui/screens/demo.dart';
+import 'package:todo_app_new_edition/ui/theme.dart';
+import 'package:todo_app_new_edition/ui/widgets/btm_nav/navigation.dart';
+import 'package:todo_app_new_edition/ui/widgets/button.dart';
+import 'package:todo_app_new_edition/ui/add_task_bar.dart';
+import 'package:todo_app_new_edition/ui/widgets/category_list.dart';
+import 'package:todo_app_new_edition/ui/widgets/side_menu.dart';
+import 'package:todo_app_new_edition/ui/widgets/task_tile.dart';
+import 'package:todo_app_new_edition/ui/details.dart';
+import 'package:todo_app_new_edition/utils/icons.dart';
+
+class AllTaskPage extends StatefulWidget {
+  const AllTaskPage({Key? key}) : super(key: key);
+
+  @override
+  State<AllTaskPage> createState() => _AllTaskPageState();
+}
+
+class _AllTaskPageState extends State<AllTaskPage> {
+  DateTime _selectedDate = DateTime.now();
+
+  var db = new Mysql();
+  final _taskController = Get.put(TaskController());
+  var notifyHelper;
+
+  // TODO -- NEW ADDED for menu bar
+  final PageController pageController = PageController();
+  int currentIndex = 0;
+
+  void onIndexChanged(int index) {
+    setState(() {
+      currentIndex = index;
+      Get.to(pages[index]);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.initializeNotification(); // initialize
+    notifyHelper.requestIOSPermissions();
+    setState(() {
+      _taskController.getTasks();
+      print("Initialize");
+    });
+  }
+
+  List pages = [
+    EntryPoint(),
+    Calendar(),
+    AllTaskPage(),
+    DemoPage(title: "3"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    print("All Task Page");
+    return Scaffold(
+      appBar: _appBar(),
+      backgroundColor: context.theme.backgroundColor,
+      // using for the two columns on the top to show Time, date and add task bar
+      body: Column(
+        children: [
+          _addTaskBar(),
+          // _addDateBar(),
+          const SizedBox(
+            height: 10,
+          ),
+          CategoryList(),
+          const SizedBox(
+            height: 10,
+          ),
+          _showTasks(),
+        ],
+      ),
+      bottomNavigationBar: BuildNavigation(
+        currentIndex: currentIndex,
+        onTap: onIndexChanged, // 切换tab事件
+        items: [
+          NavigationItemModel(
+            label: "All Task",
+            icon: SvgIcon.layout,
+          ),
+          NavigationItemModel(
+            label: "Calendar",
+            icon: SvgIcon.calendar,
+          ),
+          NavigationItemModel(
+            label: "Tag",
+            icon: SvgIcon.tag,
+          ),
+          NavigationItemModel(
+            label: "user",
+            icon: SvgIcon.user,
+            count: 3,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: menuIconColor,
+        onPressed: () {
+          Get.to(() => const AddTaskPage());
+        },
+        // TODO FIND OUT HOW TO CHANGE THE ADD Button to purple color
+        child: const Icon(Icons.add_circle_rounded, size: 50),
+      ),
+      // float button
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.centerDocked, //控制浮动按钮停靠在底部中间位置
+    );
+  }
+
+  _showTasks() {
+    return Expanded(
+      child: Obx(() {
+        return ListView.builder(
+            itemCount: _taskController.taskList.length,
+            itemBuilder: (_, index) {
+              // print(_taskController.taskList.length);
+              Task task = _taskController.taskList[index]; // pass an instance
+              // Tasks display logic by Date
+              DateTime weeklyDate =
+                  DateFormat.yMd().parse(task.date.toString());
+              var weeklyTime = DateFormat("EEEE").format(weeklyDate);
+
+              // Daily task remind
+              if (task.repeat == "Daily") {
+                DateTime date =
+                    DateFormat.jm().parse(task.startTime.toString());
+                var myTime = DateFormat("HH:mm").format(date);
+                notifyHelper.scheduledNotification(
+                    int.parse(myTime.toString().split(":")[0]), // hours
+                    int.parse(myTime.toString().split(":")[1]), // minutes
+                    task);
+                return AnimationConfiguration.staggeredList(
+                    position: index,
+                    child: SlideAnimation(
+                      child: FadeInAnimation(
+                        child: Row(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                _showBottomSheet(context, task);
+                              },
+                              child: TaskTile(task),
+                            )
+                          ],
+                        ),
+                      ),
+                    ));
+              }
+
+              // TODO ??? Weekly? Montly?
+              if (task.date == DateFormat.yMd().format(_selectedDate)) {
+                return AnimationConfiguration.staggeredList(
+                    position: index,
+                    child: SlideAnimation(
+                      child: FadeInAnimation(
+                        child: Row(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                _showBottomSheet(context, task);
+                              },
+                              child: TaskTile(task),
+                            )
+                          ],
+                        ),
+                      ),
+                    ));
+              }
+              // Weekly task remind
+              else if (task.repeat == 'Weekly' &&
+                  weeklyTime == DateFormat.EEEE().format(_selectedDate)) {
+                DateTime date =
+                    DateFormat.jm().parse(task.startTime.toString());
+                var myTime = DateFormat("HH:mm").format(date);
+                notifyHelper.repeatWeeklyNotification(
+                    int.parse(myTime.toString().split(":")[0]), // hours
+                    int.parse(myTime.toString().split(":")[1]), // minutes
+                    task);
+                return AnimationConfiguration.staggeredList(
+                    position: index,
+                    child: SlideAnimation(
+                      child: FadeInAnimation(
+                        child: Row(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                _showBottomSheet(context, task);
+                              },
+                              child: TaskTile(task),
+                            )
+                          ],
+                        ),
+                      ),
+                    ));
+              }
+              else {
+                return Container(); // cannot find any match date
+              }
+            });
+      }),
+    );
+  }
+
+  /* used to show the task state: Task Completed / Delete Task
+  * */
+  _showBottomSheet(BuildContext context, Task task) {
+    Get.bottomSheet(Container(
+      padding: EdgeInsets.only(top: 4),
+      // judge the BottomSheet height by the variable: isCompleted 0/1
+      height: task.isCompleted == 1
+          ? MediaQuery.of(context).size.height * 0.24
+          : MediaQuery.of(context).size.height * 0.32,
+      color: Get.isDarkMode ? darkGreyClr : Colors.white,
+      child: Column(
+        children: [
+          Container(
+            height: 6,
+            width: 120,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Get.isDarkMode ? Colors.grey[600] : Colors.grey[300],
+            ),
+          ),
+          Spacer(),
+          task.isCompleted == 1
+              ? Container()
+              : _bottomSheetButton(
+                  label: "Task Completed",
+                  // TODO -- Add warning message to avoid wrong selection
+                  onTap: () {
+                    _taskController.markTaskCompleted(task.id!); // UPDATE
+                    Get.back();
+                  },
+                  clr: primaryClr,
+                  context: context,
+                ),
+          _bottomSheetButton(
+            label: "Delete Task",
+            onTap: () {
+              // TODO -- Add warning message to avoid wrong deletion
+              _taskController.delete(task); // DELETE
+              Get.back();
+            },
+            clr: Colors.red[400]!,
+            context: context,
+          ),
+          SizedBox(
+            height: 22,
+          ),
+          _bottomSheetButton(
+            label: "Details",
+            // TODO --- jump to Details page
+            onTap: () async {
+              await Get.to(() => TaskDetailPage(task: task));
+              _taskController.getTasks();
+            },
+            clr: Colors.white,
+            isClose: true,
+            // set as ture
+            context: context,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
+    ));
+  }
+
+  _bottomSheetButton({
+    required String label,
+    required Function()? onTap,
+    required Color clr,
+    bool isClose = false,
+    required BuildContext context,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 4.0),
+        height: 55,
+        width: MediaQuery.of(context).size.width * 0.9,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 2,
+            color: isClose == true
+                ? Get.isDarkMode
+                    ? Colors.grey[600]!
+                    : Colors.grey[350]!
+                : clr,
+          ),
+          borderRadius: BorderRadius.circular(20),
+          color: isClose == true ? Colors.transparent : clr,
+        ),
+        child: Center(
+          child: Text(
+            label,
+            // TODO copyWith() -- COPY ALL THE PROPERTY OF THE INSTANCE AND CHANGE SOME
+            style:
+                isClose ? titleStyle : titleStyle.copyWith(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // rebuilt the Container() in _appTaskBar
+  _addTaskBar() {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // wrap Column with a container so that can add padding, margin..
+          Container(
+            // margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, // margin left
+              children: [
+                // you can change the time showing format by DateFormat.yMMMd()
+                Text(
+                  DateFormat.yMMMd().format(DateTime.now()),
+                  style: subHeadingStyle,
+                ),
+                Text(
+                  "Today",
+                  style: headingStyle,
+                ),
+              ],
+            ),
+          ),
+          MyButton(
+              label: "+ Add Task",
+              onTap: () async {
+                // TODO !!! IMPORTANT FOR HOMEPAGE DISPLAY
+                await Get.to(() => AddTaskPage());
+                _taskController.getTasks();
+              }) // Get.to: jump to a new page
+        ],
+      ),
+    );
+  }
+
+  _appBar() {
+    return AppBar(
+      elevation: 0, // eliminate the shadow of header banner
+      backgroundColor: context.theme.backgroundColor,
+      leading: GestureDetector(
+        onTap: () {
+          // TODO -- Logic for theme change
+          ThemeServices().switchTheme();
+          notifyHelper.displayNotification(
+            title: "Theme changed",
+            body: Get.isDarkMode
+                ? "Activated Light Theme"
+                : "Activated Dark Theme",
+          );
+          // notifyHelper.scheduledNotification();
+        },
+        child: Icon(
+          // Day and moon icon should change according to the Theme Mode
+          Get.isDarkMode ? Icons.wb_sunny_outlined : Icons.nightlight_rounded,
+          size: 20,
+          // Icon color should change according to the Theme Mode
+          color: Get.isDarkMode ? Colors.white : Colors.black,
+        ),
+      ),
+      actions: [
+        // Icon(
+        //   Icons.person,
+        //   // Icon color should change according to the Theme Mode
+        //   color: Get.isDarkMode ? Colors.white : Colors.black,
+        // ),
+        // 头像png控件
+        // CircleAvatar(
+        //   backgroundImage: AssetImage(
+        //     "images/header.png"
+        //   ),
+        // ),
+        SizedBox(
+          width: 20,
+        )
+      ],
+    );
+  }
+}
